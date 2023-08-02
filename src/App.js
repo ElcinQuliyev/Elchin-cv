7e19cac242f0159d5f3df73d21c5d5cfbd25d871
ElcinQuluyev <elcin-quliyev-2022@mail.ru>
2023-08-02 13:02:46 +0400

    cv added

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+
+import { Box, Flex, Heading } from '@chakra-ui/react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import Blogs from './modules/Blogs/Blogs';
+import Main from './modules/Main/Main';
+import Projects from './modules/Projects/Projects';
+import { ROUTES } from './routes';
+
+function App() {
+  return (
+    <Flex  w="95%" mx="auto" mt={4} columnGap={6} >
+      <MainLayout/>
+      <Box w="68%" bg="blue" color="white"> 
+          <Routes>
+         
+            <Route path="" element={<Navigate to={ROUTES.home}/>} />
+            <Route path={ROUTES.home} element={<Main/>} />
+            <Route path={ROUTES.blogs} element={<Blogs/>} />
+            <Route path={ROUTES.projects} element={<Projects/>} />
+            
+            <Route path="*" element={<Heading as="h1" >404 Not Found</Heading>} />
+          </Routes>
+      </Box>
+    </Flex>
+  );
+}
+
+export default App;
