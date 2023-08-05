d7ccfac1bd5b630e5837fe40e6eac403cc5dd534
ElcinQuluyev <elcinq743@gmail.com>
2023-08-05 13:28:18 +0400

    push

--- src/services/ProfileService.js
+++ src/services/ProfileService.js
@@ -1,28 +1,28 @@
 import axios from "axios";
-const baseURL = "http://localhost:4000/api/user";
+const baseURL = "https://el-db.vercel.app";
 
 export async function getProfile() {
-  const req = await axios.get("", {
+  const req = await axios.get("userData", {
     baseURL,
   });
   return req.data;
 }
 
 export async function getExperience() {
-  const req = await axios.get("/experience", {
+  const req = await axios.get("/Experience", {
     baseURL,
   });
   return req.data;
 }
 
 export async function getEducation() {
-  const req = await axios.get("/education", {
+  const req = await axios.get("/Education", {
     baseURL,
   });
   return req.data;
 }
 export async function getTech() {
-  const req = await axios.get("/tech", {
+  const req = await axios.get("/Tech", {
     baseURL,
   });
   return req.data;
